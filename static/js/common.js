57980b7907dec26511432ab905713d89f275d04d
liqi <liqiang@transinfo.com.cn>
2022-03-21 17:11:17 +0800

    ui初始化

---
+++ static/js/common.js
@@ -0,0 +1,39 @@
+/*include.html 文件中的header 和footer模块引用*/
+var project_name = "/ruoyi-ui/static"
+
+    document.write("<meta charset='utf-8'>");
+    document.write("<meta name='viewport' content='width=device-width, initial-scale=1.0'>");
+    document.write("<meta http-equiv='X-UA-Compatible' content='IE=edge'>");
+    document.write("<meta name='keywords' content=''>");
+    document.write("<meta name='description' content=''>");
+    document.write("<link href=\'"+project_name+"/css/bootstrap.min.css?v=3.3.7}' rel='stylesheet'/>");
+    document.write("<link href=\'"+project_name+"/css/font-awesome.min.css?v=4.7.3}' rel='stylesheet'/>");
+    document.write("<!-- bootstrap-table 表格插件样式 -->");
+    document.write("<link href=\'"+project_name+"/ajax/libs/bootstrap-table/bootstrap-table.min.css?v=1.18.3}' rel='stylesheet'/>");
+    document.write("<link href=\'"+project_name+"/css/animate.min.css?v=20210831}' rel='stylesheet'/>");
+    document.write("<link href=\'"+project_name+"/css/style.min.css?v=20210831}' rel='stylesheet'/>");
+    document.write("<link href=\'"+project_name+"/ruoyi/css/ry-ui.css?v=4.7.3}' rel='stylesheet'/>");
+
+
+
+
+    document.write("<a id='scroll-up' href='#' class='btn btn-sm display'><i class='fa fa-angle-double-up'></i></a>");
+    document.write("<script src=\'"+project_name+"/js/jquery.min.js?v=3.6.0'></script>");
+    document.write("<script src=\'"+project_name+"/js/bootstrap.min.js?v=3.3.7'></script>");
+    document.write("<!-- bootstrap-table 表格插件 -->");
+    document.write("<script src=\'"+project_name+"/ajax/libs/bootstrap-table/bootstrap-table.min.js?v=1.18.3'></script>");
+    document.write("<script src=\'"+project_name+"/ajax/libs/bootstrap-table/locale/bootstrap-table-zh-CN.min.js?v=1.18.3'></script>");
+    document.write("<script src=\'"+project_name+"/ajax/libs/bootstrap-table/extensions/mobile/bootstrap-table-mobile.js?v=1.18.3'></script>");
+    document.write("<!-- jquery-validate 表单验证插件 -->");
+    document.write("<script src=\'"+project_name+"/ajax/libs/validate/jquery.validate.min.js?v=1.19.3'></script>");
+    document.write("<script src=\'"+project_name+"/ajax/libs/validate/jquery.validate.extend.js?v=1.19.3'></script>");
+    document.write("<script src=\'"+project_name+"/ajax/libs/validate/messages_zh.js?v=1.19.3'></script>");
+    document.write("<!-- bootstrap-table 表格树插件 -->");
+    document.write("<script src=\'"+project_name+"/ajax/libs/bootstrap-table/extensions/tree/bootstrap-table-tree.min.js?v=1.18.3'></script>");
+    document.write("<!-- 遮罩层 -->");
+    document.write("<script src=\'"+project_name+"/ajax/libs/blockUI/jquery.blockUI.js?v=2.70.0'></script>");
+    document.write("<script src=\'"+project_name+"/ajax/libs/iCheck/icheck.min.js?v=1.0.3'></script>");
+    document.write("<script src=\'"+project_name+"/ajax/libs/layer/layer.min.js?v=3.5.1'></script>");
+    document.write("<script src=\'"+project_name+"/ajax/libs/layui/layui.min.js?v=2.6.8'></script>");
+    document.write("<script src=\'"+project_name+"/ruoyi/js/common.js?v=4.7.3'></script>");
+    document.write("<script src=\'"+project_name+"/ruoyi/js/ry-ui.js?v=4.7.3'></script>");
